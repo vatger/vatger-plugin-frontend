58e749522d4ddff22e9b7eca96c06626218ec517
Ben Böckmann <boeckmannben@gmail.com>
2026-04-23 21:37:17 +0200

    vite config updated

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,32 @@
-import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
+import { loadEnv } from "vite"
 import { defineConfig } from "vite"
+import path from "path"
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "")
+
+  return {
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
+    server: {
+      open: true,
+      proxy: {
+        "/api": {
+          target: env.VITE_API_URL || "http://localhost:8000",
+          ws: true,
+        },
+        "/static": env.VITE_API_URL || "http://localhost:8000",
+      },
+    },
+    build: {
+      outDir: "build",
+      sourcemap: true,
+    },
+  }
 })
